e9f76d78128ff1894f4d6b93a7d06d0a01771c68
Jennifer <Jennifer@192.168.0.11>
2017-11-19 15:52:49 +0100

    Merge origin/master
    Conflicts:
    	Scrum/src/scrum/Controleur/CtrlFormMicroplaque.java
    	Scrum/src/scrum/Interface/FormFirstRead.java
    	Scrum/src/scrum/Interface/FormMicroplaque.java
    	Scrum/src/scrum/Interface/MenuPrincipal.java

--- Cinematheque/src/java/Cinema/UtilisateurCtrl.java
+++ Cinematheque/src/java/Cinema/UtilisateurCtrl.java
@@ -10,29 +10,32 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *
  * @author Jennifer
  */
-
 @Named(value = "UtilisateurCtrl")
 @ViewScoped
-public class UtilisateurCtrl implements Serializable{
-    
+public class UtilisateurCtrl extends HttpServlet implements Serializable {
+
     @EJB
-    private UtilisateurDAO dao; 
-    private Utilisateur selectedUser; 
-    
+    private static final long serialVersionUID=1L;
+    private UtilisateurDAO dao;
+    private Utilisateur selectedUser;
+
     public UtilisateurCtrl() {
     }
-   
-    
+
     public List<Utilisateur> getUser() {
         return dao.allUser();
     }
-    
+
     public void addUser(Utilisateur u) {
         dao.add(u);
     }
@@ -52,16 +55,16 @@ public class UtilisateurCtrl implements Serializable{
     public void setUser(Utilisateur selectedUser) {
         this.selectedUser = selectedUser;
     }
-    
-    public void deleteUtilisateur(){
+
+    public void deleteUtilisateur() {
         int id = selectedUser.getIduser();
         dao.removeUtilisateur(id);
     }
-    
-    public void modifUtilisateur(){
+
+    public void modifUtilisateur() {
         dao.updateUtilisateur(this.selectedUser);
     }
-    
+
 //    public void chercheUtilisateur(String nomu, String mdp){
 //        try 
 //        {
@@ -78,15 +81,17 @@ public class UtilisateurCtrl implements Serializable{
 //        {
 //          System.out.println("Ce nom n'est pas connu de la base de données");
 //        }
-        
-        
-      
-        // Catch sur l'utilisateur pour savoir si l'utilisateur existe
-        // Vérification de la cohérence du mot de passe
-      
-   
-      
-    
+    // Catch sur l'utilisateur pour savoir si l'utilisateur existe
+    // Vérification de la cohérence du mot de passe
+    protected void doGet(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/connexion.xhtml").forward(requete, reponse);
     }
-    
 
+    protected void doPost(HttpServletRequest requete, HttpServletResponse reponse) throws ServletException, IOException {
+        ConnectionForm form = new ConnectionForm();
+        form.verifierIdentifiant(requete);
+        requete.setAttribute("form", form);
+        this.getServletContext().getRequestDispatcher("/WEB-INF/connexion.xhtml").forward(requete, reponse);
+    }
+
+}
